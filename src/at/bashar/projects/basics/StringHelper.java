6e761f9ba2d6826e114aaa46f276edee0d86e669
basharkh41 <bashar.khalil2003@gmail.com>
2022-05-06 11:37:13 +0200

    angefangen, systemoutprints, methode erstellen noch

--- src/at/bashar/projects/basics/StringHelper.java
+++ src/at/bashar/projects/basics/StringHelper.java
@@ -1,5 +1,34 @@
 package at.bashar.projects.basics;
+import java.util.Scanner;
 
 public class StringHelper {
+    public static void main(String[] args) {
+        System.out.println("Please enter the text");
+        Scanner a = new Scanner(System.in);
+        String input = a.nextLine();
+        System.out.println("Hallo " + input + " what do you want to do with your text?");
+        System.out.println("1. is my text a Palindrome?");
+        System.out.println("how many letters does my text contain?");
+        System.out.println("i want to reverse my text!");
+        System.out.println("how many words does my text contain?");
 
+
+
+        public static boolean isPalindrom(String text){
+
+        }
+        public static int countLetters(String text, char c){
+
+        }
+        public static String reverseString(String text){
+
+
+        }
+
+        public static int getAmountOfLetters(String text){
+
+        }
+
+
+    }
 }
